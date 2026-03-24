0a3d6aa6d06ae1eaf5c7d7d182ab508bbaf3ee6b
Nikhil <1837nik@gmail.com>
2026-03-24 11:39:18 +0530

    video2

---
+++ video2.py
@@ -0,0 +1,23 @@
+# Python Day-2 - Variables
+
+# Variables is like containers to store data with specific name
+# Python is a case sensitive language
+
+# Example
+name = "Rohit"
+
+print(name) # Correct
+
+
+'''
+Wrong methods to create Variables:
+1. Start with numbers
+    23name = "rohit" ---> Wrong
+    name43 = "aman" ---> Correct
+
+2. Start with any symbol except '_'
+    @age = 15 ---> Wrong
+    age* = 15 ---> Wrong also
+    _age = 17 ---> Correct
+    age_ = 18 ---> Correct
+'''
